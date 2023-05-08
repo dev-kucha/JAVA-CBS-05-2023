c0868fa2c9a14204db18c445a355d6886ee4aa00
kucha <ukucher@gmail.com>
2023-05-09 00:08:36 +0300

    Рядки додано

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Кучер Юрій Валерійович");
+        System.out.println("10.03.1976");
+        System.out.println("Майбутній JAVA developer :-)");
+    }
+}
